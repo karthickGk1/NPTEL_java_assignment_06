69bddcaf36be93707177afdad2d5579c18c8b039
iamkarthick.G <101092386+karthickGk1@users.noreply.github.com>
2023-08-12 18:55:58 +0530

    Add files via upload

---
+++ karthick.java
@@ -0,0 +1,18 @@
+public class karthick{ 
+    public static void main(String[] args) { 
+		// Object of the main class is created
+		Question23 q = new Question23();
+		// Print method on object of Question class is called
+		q.studentMethod();
+    }
+	
+	// 'print()' method is defined in class Question
+	void print(Question23 object){
+		System.out.print("Well Done!");
+	}
+// Define a method named 'studentMethod()' in class Question
+void studentMethod(){
+  print(this);
+// Call the method named 'print()' in class Question
+}
+}
